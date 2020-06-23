27155305ffa6eab20320ef1052914f00512ec0ef
lky1020 <57024183+lky1020@users.noreply.github.com>
2020-06-23 20:09:25 +0800

    Update P1

--- Practical/P1/P1/P1.cpp
+++ Practical/P1/P1/P1.cpp
@@ -6,9 +6,14 @@
 #define WINDOW_TITLE "P1 - Intro"
 
 //Function
+void triangle();
+void lineLoopTriangle();
+void pointTriangle();
+void quad();
 void pahangFlag();
 void negeriSembilanFlag();
 void englandFlag();
+void scotlandFlag();
 
 LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -71,15 +76,87 @@ void display()
 	//glClear(GL_COLOR_BUFFER_BIT);	//is used to clear the black background
 
 	//Triangle
-	/*glLineWidth(10);	//set width of line (default = 1)
+	//triangle();
+
+	//Line Loop Triangle
+	//lineLoopTriangle();
+
+	//Point Triangle
+	//pointTriangle();
+
+	//Quads
+	//quad();
+
+	//Practical 1 exercise
+	glClearColor(0.0f, 1.0f, 1.0f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	//Pahang Flag
+	pahangFlag();
+
+	//Negeri Sembilan Flag
+	negeriSembilanFlag();
+
+	//England Falg
+	englandFlag();
+
+	//Scotland Flag
+	scotlandFlag();
+
+	//--------------------------------
+	//	End of OpenGL drawing
+	//--------------------------------
+}
+//--------------------------------------------------------------------
+
+void triangle() {
 	glBegin(GL_TRIANGLES);	//GL_TRIANGLES create triangle, //GL_LINE_LOOP create line
 	glVertex2f(-0.5f, -0.5f);
 	glVertex2f(0.0f, 0.5f);
 	glVertex2f(0.5f, -0.5f);
-	glEnd();*/
+	glEnd();
+}
 
-	//Quads
-	/*glBegin(GL_QUADS);
+void lineLoopTriangle() {
+	glLineWidth(10.0);	//set width of line (default = 1)
+	glBegin(GL_LINE_LOOP);	//GL_TRIANGLES create triangle, //GL_LINE_LOOP create line
+	
+	//Red vertex
+	glColor3d(1.0f, 0.0f, 0.0f);
+	glVertex2f(-0.5f, -0.5f);
+
+	//Green vertex
+	glColor3d(0.0f, 1.0f, 0.0f);
+	glVertex2f(0.0f, 0.5f);
+
+	//Blue vertex
+	glColor3d(0.0f, 0.0f, 1.0f);
+	glVertex2f(0.5f, -0.5f);
+	glEnd();
+}
+
+void pointTriangle() {
+
+	//Point is based on the pixel(so point is square)
+	glPointSize(25.0);
+	glBegin(GL_POINTS);
+
+	//Red vertex
+	glColor3d(1.0f, 0.0f, 0.0f);
+	glVertex2f(-0.5f, -0.5f);
+
+	//Green vertex
+	glColor3d(0.0f, 1.0f, 0.0f);
+	glVertex2f(0.0f, 0.5f);
+
+	//Blue vertex
+	glColor3d(0.0f, 0.0f, 1.0f);
+	glVertex2f(0.5f, -0.5f);
+	glEnd();
+}
+
+void quad() {
+	glBegin(GL_QUADS);
 
 	//Red vertex
 	glColor3d(1.0f, 0.0f, 0.0f);
@@ -97,70 +174,161 @@ void display()
 	glColor3d(1.0f, 0.0f, 1.0f);
 	glVertex2f(0.5f, -0.5f);
 
-	glEnd();*/
-
-	//Pahang Flag
-	//pahangFlag();
-
-	//Negeri Sembilan Flag
-	//negeriSembilanFlag();
-
-	//--------------------------------
-	//	End of OpenGL drawing
-	//--------------------------------
+	glEnd();
 }
-//--------------------------------------------------------------------
 
 void pahangFlag() {
-	glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
 
 	glBegin(GL_QUADS);
 	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(-1.0f, 0.5f);
+	glVertex2f(-1.0f, 0.375f);
+	glVertex2f(-0.5f, 0.375f);
 	glVertex2f(-0.5f, 0.5f);
-	glVertex2f(0.5f, 0.5f);
-	glVertex2f(0.5f, 0.0f);
-	glVertex2f(-0.5f, 0.0f);
 	glEnd();
 
 	glBegin(GL_QUADS);
 	glColor3f(0.0f, 0.0f, 0.0f);
-	glVertex2f(-0.5f, 0.0f);
-	glVertex2f(0.5f, 0.0f);
-	glVertex2f(0.5f, -0.5f);
-	glVertex2f(-0.5f, -0.5f);
+	glVertex2f(-1.0f, 0.375f);
+	glVertex2f(-1.0f, 0.25f);
+	glVertex2f(-0.5f, 0.25f);
+	glVertex2f(-0.5f, 0.375f);
 	glEnd();
 }
 
 void negeriSembilanFlag() {
-	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
+
 
 	glBegin(GL_QUADS);
 	glColor3f(1.0f, 1.0f, 0.0f);
-	glVertex2f(-0.8f, 0.5f);
-	glVertex2f(0.8f, 0.5f);
-	glVertex2f(0.8f, -0.5f);
-	glVertex2f(-0.8f, -0.5f);
+	glVertex2f(-0.25f, 0.5f);
+	glVertex2f(-0.25f, 0.25f);
+	glVertex2f(0.25f, 0.25f);
+	glVertex2f(0.25f, 0.5f);
 	glEnd();
 
 	glBegin(GL_TRIANGLES);
 	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex2f(-0.8f, 0.5f);
+	glVertex2f(-0.25f, 0.5f);
+	glVertex2f(0.0f, 0.375f);
 	glVertex2f(0.0f, 0.5f);
-	glVertex2f(0.0f, 0.0f);
 	glEnd();
 
 	glBegin(GL_TRIANGLES);
 	glColor3f(0.0f, 0.0f, 0.0f);
-	glVertex2f(-0.8f, 0.5f);
-	glVertex2f(-0.8f, 0.0f);
-	glVertex2f(0.0f, 0.0f);
+	glVertex2f(-0.25f, 0.5f);
+	glVertex2f(-0.25f, 0.375f);
+	glVertex2f(0.0f, 0.375f);
 	glEnd();
 }
 
 void englandFlag() {
+	glBegin(GL_QUADS);
+	//top left (white)
+	glColor3d(0.5, 0.5, 0.5);
+	glVertex2f(0.5, 0.5);
 
+	glColor3d(1.0, 1.0, 1.0);
+	glVertex2f(0.5, 0.4);
+	glVertex2f(0.725, 0.4);
+	glVertex2f(0.725, 0.5);
+
+	//top right (white)
+	glVertex2f(0.775, 0.5);
+	glVertex2f(0.775, 0.4);
+	glVertex2f(1.0, 0.4);
+
+	glColor3d(0.5, 0.5, 0.5);
+	glVertex2f(1.0, 0.5);
+
+	//bottom left (white)
+	glColor3d(1.0, 1.0, 1.0);
+	glVertex2f(0.5, 0.35);
+
+	glColor3d(0.5, 0.5, 0.5);
+	glVertex2f(0.5, 0.25);
+
+	glColor3d(1.0, 1.0, 1.0);
+	glVertex2f(0.725, 0.25);
+	glVertex2f(0.725, 0.35);
+
+	//bottom right (white)
+	glVertex2f(0.775, 0.35);
+	glVertex2f(0.775, 0.25);
+
+	glColor3d(0.5, 0.5, 0.5);
+	glVertex2f(1.0, 0.25);
+
+	glColor3d(1.0, 1.0, 1.0);
+	glVertex2f(1.0, 0.35);
+
+	glColor3d(0.57, 0.0, 0.0); //red
+
+	//left banner (red)
+	glVertex2f(0.5, 0.40);
+	glVertex2f(0.5, 0.35);
+	glVertex2f(0.725, 0.35);
+	glVertex2f(0.725, 0.40);
+
+	//down banner (red)
+	glVertex2f(0.725, 0.35);
+	glVertex2f(0.725, 0.25);
+	glVertex2f(0.775, 0.25);
+	glVertex2f(0.775, 0.35);
+
+	//right banner (red)
+	glVertex2f(0.775, 0.4);
+	glVertex2f(0.775, 0.35);
+	glVertex2f(1.0, 0.35);
+	glVertex2f(10., 0.4);
+
+	//up banner (red)
+	glVertex2f(0.725, 0.5);
+	glVertex2f(0.725, 0.4);
+	glVertex2f(0.775, 0.4);
+	glVertex2f(0.775, 0.5);
+
+	//middle square (red)
+	glVertex2f(0.725, 0.4);
+	glVertex2f(0.725, 0.35);
+	glVertex2f(0.775, 0.35);
+	glVertex2f(0.775, 0.4);
+	glEnd();
+
+}
+
+void scotlandFlag() {
+	glBegin(GL_QUADS);
+	glColor3d(1.0, 1.0, 1.0); //white
+	glVertex2f(-1.0, -0.25);
+	glVertex2f(-1.0, -0.5);
+	glVertex2f(-0.5, -0.5);
+	glVertex2f(-0.5, -0.25);
+	glEnd();
+
+	glColor3d(0.0, 0.4, 0.74); //blue
+
+	glBegin(GL_TRIANGLES);
+	//left
+	glVertex2f(-1.0, -0.3);
+	glVertex2f(-1.0, -0.45);
+	glVertex2f(-0.80, -0.375);
+
+	//down
+	glVertex2f(-0.75, -0.4);
+	glVertex2f(-0.95, -0.5);
+	glVertex2f(-0.55, -0.5);
+
+	//right
+	glVertex2f(-0.7, -0.375);
+	glVertex2f(-0.5, -0.45);
+	glVertex2f(-0.5, -0.3);
+
+	//up
+	glVertex2f(-0.95, -0.25);
+	glVertex2f(-0.75, -0.35);
+	glVertex2f(-0.55, -0.25);
+	glEnd();
 }
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow)
