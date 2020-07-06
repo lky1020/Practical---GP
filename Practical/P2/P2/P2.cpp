066b28610a641117dca596a0d032190bcdd0d8f8
lky1020 <57024183+lky1020@users.noreply.github.com>
2020-07-06 19:09:30 +0800

    Build P2

---
+++ Practical/P2/P2/P2.cpp
@@ -0,0 +1,225 @@
+#include <Windows.h>
+#include <gl/GL.h>
+
+#pragma comment (lib, "OpenGL32.lib")
+
+#define WINDOW_TITLE "OpenGL Window"
+
+int num = 0;
+
+void q1();
+void q2();
+void q3();
+void q4();
+
+LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE) PostQuitMessage(0);
+		else if (wParam == VK_NUMPAD1 || wParam == 0x31) num = 1;
+		else if (wParam == VK_NUMPAD2 || wParam == 0x32) num = 2;
+		else if (wParam == VK_NUMPAD3 || wParam == 0x33) num = 3;
+		else if (wParam == VK_NUMPAD4 || wParam == 0x34) num = 4;
+		break;
+
+	default:
+		break;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+//--------------------------------------------------------------------
+
+bool initPixelFormat(HDC hdc)
+{
+	PIXELFORMATDESCRIPTOR pfd;
+	ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+
+	pfd.cAlphaBits = 8;
+	pfd.cColorBits = 32;
+	pfd.cDepthBits = 24;
+	pfd.cStencilBits = 0;
+
+	pfd.dwFlags = PFD_DOUBLEBUFFER | PFD_SUPPORT_OPENGL | PFD_DRAW_TO_WINDOW;
+
+	pfd.iLayerType = PFD_MAIN_PLANE;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd.nVersion = 1;
+
+	// choose pixel format returns the number most similar pixel format available
+	int n = ChoosePixelFormat(hdc, &pfd);
+
+	// set pixel format returns whether it sucessfully set the pixel format
+	if (SetPixelFormat(hdc, n, &pfd))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+//--------------------------------------------------------------------
+
+void display()
+{
+	//--------------------------------
+	//	OpenGL drawing
+	//--------------------------------
+	switch (num) {
+	case 1:
+		q1();
+		break;
+	case 2:
+		q2();
+		break;
+	case 3:
+		q3();
+		break;
+	case 4:
+		q4();
+		break;
+	default:
+		q4();
+		break;
+	}
+
+	//--------------------------------
+	//	End of OpenGL drawing
+	//--------------------------------
+}
+//--------------------------------------------------------------------
+
+void q1() {
+	//glTranslatef(0.0001f, 0.0, 0.0);
+	glTranslatef(0.2f, 0.0, 0.0);
+	glBegin(GL_TRIANGLES);
+	glVertex2f(0.0, 0.0);
+	glVertex2f(0.1, 0.0);
+	glVertex2f(0.0, 0.1);
+	glEnd();
+}
+
+void q2() {
+	glLoadIdentity();
+	//glTranslatef(0.0001f, 0.0, 0.0);
+	glTranslatef(0.5f, 0.0, 0.0);
+	glBegin(GL_TRIANGLES);
+	glVertex2f(0.0, 0.0);
+	glVertex2f(0.5, 0.0);
+	glVertex2f(0.0, 0.5);
+	glEnd();
+}
+
+void q3() {
+	/*//original
+	glColor3d(1.0, 1.0, 1.0);
+	glLoadIdentity();
+	glBegin(GL_TRIANGLES);
+	glVertex2f(0.0, 0.0);
+	glVertex2f(0.5, 0.0);
+	glVertex2f(0.0, 0.5);
+	glEnd();
+
+	//translate then rotate
+	glColor3d(0.0, 0.0, 1.0);
+	glLoadIdentity();
+	glTranslatef(0.5f, 0.0, 0.0);
+	glRotatef(45.0, 0.0, 0.0, 1.0);
+	glBegin(GL_TRIANGLES);
+	glVertex2f(0.0, 0.0);
+	glVertex2f(0.5, 0.0);
+	glVertex2f(0.0, 0.5);
+	glEnd();*/
+
+	//rotate the translate
+	glColor3d(0.0, 1.0, 1.0);
+	glLoadIdentity();
+	glRotatef(45.0, 0.0, 0.0, 1.0);
+	glTranslatef(0.5f, 0.0, 0.0);
+	glBegin(GL_TRIANGLES);
+	glVertex2f(0.0, 0.0);
+	glVertex2f(0.5, 0.0);
+	glVertex2f(0.0, 0.5);
+	glEnd();
+}
+
+void q4() {
+	q3();
+	glLoadIdentity();
+	glBegin(GL_QUADS);
+	glVertex2f(-0.5, -0.5);
+	glVertex2f(-0.5, -1.0);
+	glVertex2f(0.0, -1.0);
+	glVertex2f(0.0, -0.5);
+	glEnd();
+}
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow)
+{
+	WNDCLASSEX wc;
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.hInstance = GetModuleHandle(NULL);
+	wc.lpfnWndProc = WindowProcedure;
+	wc.lpszClassName = WINDOW_TITLE;
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+
+	if (!RegisterClassEx(&wc)) return false;
+
+	HWND hWnd = CreateWindow(WINDOW_TITLE, WINDOW_TITLE, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+		NULL, NULL, wc.hInstance, NULL);
+
+	//--------------------------------
+	//	Initialize window for OpenGL
+	//--------------------------------
+
+	HDC hdc = GetDC(hWnd);
+
+	//	initialize pixel format for the window
+	initPixelFormat(hdc);
+
+	//	get an openGL context
+	HGLRC hglrc = wglCreateContext(hdc);
+
+	//	make context current
+	if (!wglMakeCurrent(hdc, hglrc)) return false;
+
+	//--------------------------------
+	//	End initialization
+	//--------------------------------
+
+	ShowWindow(hWnd, nCmdShow);
+
+	MSG msg;
+	ZeroMemory(&msg, sizeof(msg));
+
+	while (true)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT) break;
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		display();
+
+		SwapBuffers(hdc);
+	}
+
+	UnregisterClass(WINDOW_TITLE, wc.hInstance);
+
+	return true;
+}
+//--------------------------------------------------------------------
